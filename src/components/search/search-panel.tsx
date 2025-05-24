2867a6ebe9c1add3a88a7e8f4448ebf1c617662a
Shafi Akinropo <sakinropo@gmail.com>
2025-05-24 18:59:01 +0100

    update all

--- src/components/search/search-panel.tsx
+++ src/components/search/search-panel.tsx
@@ -768,7 +768,7 @@ export const SearchPanel = ({ isMobile = false }: SearchPanelProps) => {
     >
       <form onSubmit={handleSearch} className="w-full">
         <div
-          className={`flex items-center bg-gray-100 rounded-full px-3 py-2 w-full transition-all duration-300 ${
+          className={`flex items-center border border-gray-200 rounded-2xl px-3 py-2 w-full transition-all duration-300 ${
             isSearchFocused ? "rounded-b-none shadow-md" : ""
           }`}
         >
