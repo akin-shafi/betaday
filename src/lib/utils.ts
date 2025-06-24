ce4b72e27aae1c0568e1dcabbbd9f7629e08f205
Shafi Akinropo <sakinropo@gmail.com>
2025-06-24 05:40:25 +0100

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number,
+  options: {
+    currency?: "NGN" | "USD" | "EUR" | "GBP";
+    locale?: string;
+  } = {}
+) {
+  const { currency = "NGN", locale = "en-NG" } = options;
+
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+  }).format(price);
 }
